f84d0456563dcdfe99a5c0227f448bf27ff7f05b
Tomohiko Himura <eiel.hal@gmail.com>
2014-10-07 16:34:46 +0900

    production環境のみ認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :auth
+  before_action :auth, if: -> { Rails.env.production? }
 
   private
   def auth
