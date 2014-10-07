216da526d9edaf6131795546bf94a2fcf1cf5127
Tomohiko Himura <eiel.hal@gmail.com>
2014-10-07 17:19:19 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141007065147) do
+ActiveRecord::Schema.define(version: 20141007075431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20141007065147) do
   create_table "customers", force: true do |t|
     t.string   "last_name"
     t.string   "first_name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
